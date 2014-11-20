0571b4d5616d26e34711a609650e483f9e134f25
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-11-20 15:57:19 +0000

    fix test

--- test/archive_on_fail_test.sh
+++ test/archive_on_fail_test.sh
@@ -4,7 +4,7 @@ set -eu
 
 . test/lxci_test_config
 
-$LXCI $BASE --name foo --command "exit 1" || true
+$LXCI $BASE --archive-on-fail --name foo --command "exit 1" || true
 
 ls "$ARCHIVE_CONFIG_PATH/foo" || {
     echo "The container should be in the archive!"
