35e4b0fd901dae9309051e1a159fe5ee30a27ec6
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-11-21 00:14:22 +0200

    Provision container too

--- provision.sh
+++ provision.sh
@@ -2,23 +2,37 @@
 
 set -eu
 
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 
+# Add sources for latest Jenkins
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 cat >/etc/apt/sources.list.d/jenkins.list<<EOF
 deb http://pkg.jenkins-ci.org/debian binary/
 EOF
 
-# Allow passwordless sudo for lxci command in jenkins
+# Allow passwordless sudo for lxci command for jenkins user used by the Jenkins
+# server
 cat >/etc/sudoers.d/jenkins_lxci<<EOF
 jenkins ALL=NOPASSWD:/usr/local/bin/lxci, /usr/bin/lxci
 EOF
 
+# Install LXC, Jenkins and lxci deps
 apt-get update
+apt-get install -y lxc python3-lxc jenkins rsync
 
-apt-get install -y lxc python3-lxc jenkins
 
-cd /vagrant
+# Use create a basic base container
+lxc-create --template download --name trusty-amd64 -- --dist ubuntu --release trusty --arch amd64
+
+# Install openssh-server into the container
+chroot /var/lib/lxc/trusty-amd64/rootfs apt-get install openssh-server --yes --no-install-recommends
+
 # Install lxci
-sudo make install
+if [ -f lxci.py ]; then
+    make install
+else
+    # Vagrant provision
+    cd /vagrant
+    make install
+fi
 
 
