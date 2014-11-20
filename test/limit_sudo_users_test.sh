38d52ee1988056d6eea53cf142eca87a39717872
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-11-20 16:24:29 +0000

    fix test

--- test/limit_sudo_users_test.sh
+++ test/limit_sudo_users_test.sh
@@ -12,5 +12,5 @@ export SUDO_UID=0
 $LXCI $BASE --name container --command "echo i can read secrets" --sync /tmp/lxci_test_dir && {
     echo "Should not allow sudo users to sync files from random locations"
     exit 1
-}
+} || true
 
