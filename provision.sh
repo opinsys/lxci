71ce0efda1b49e8c3b223abf8f138ad3cc20a0ef
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-11-24 21:31:12 +0200

    Use unprivileged containers for jenkins

--- provision.sh
+++ provision.sh
@@ -19,12 +19,26 @@ EOF
 apt-get update
 apt-get install -y lxc python3-lxc jenkins rsync
 
+JENKINS_HOME=/var/lib/jenkins
+sudo -u vagrant ssh-keygen -N "" -f /home/vagrant/.ssh/id_rsa
+mkdir -p /var/lib/jenkins/.ssh
+cp /home/vagrant/.ssh/id_rsa.pub $JENKINS_HOME/.ssh/authorized_keys
+
+usermod -v 296608-362144 -w 296608-362144 jenkins
+mkdir -p $JENKINS_HOME/.config/lxc
+echo "jenkins veth lxcbr0 2" >> /etc/lxc/lxc-usernet
+echo "lxc.id_map = u 0 296608 65536" > $JENKINS_HOME/.config/lxc/default.conf
+echo "lxc.id_map = g 0 296608 65536" >> $JENKINS_HOME/.config/lxc/default.conf
+echo "lxc.network.type = veth" >> $JENKINS_HOME/.config/lxc/default.conf
+echo "lxc.network.link = lxcbr0" >> $JENKINS_HOME/.config/lxc/default.conf
+chown -R jenkins:jenkins $JENKINS_HOME
+
 
 # Use create a basic base container
-lxc-create --template download --name trusty-amd64 -- --dist ubuntu --release trusty --arch amd64
+# lxc-create --template download --name trusty-amd64 -- --dist ubuntu --release trusty --arch amd64
 
 # Install openssh-server into the container
-chroot /var/lib/lxc/trusty-amd64/rootfs apt-get install openssh-server --yes --no-install-recommends
+# chroot /var/lib/lxc/trusty-amd64/rootfs apt-get install openssh-server --yes --no-install-recommends
 
 # Install lxci
 if [ -f lxci.py ]; then
@@ -36,3 +50,4 @@ else
 fi
 
 
+poweroff # Ensure jenkins user will have the namespace setup enabled
